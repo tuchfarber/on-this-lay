58f2c9d1108bf52de4f64ff82c15d194aeda9dcc
Billy <wjs.scanlon@gmail.com>
2016-08-27 00:10:30 -0400

    Changed JSON format

--- back_end/api.py
+++ back_end/api.py
@@ -4,6 +4,7 @@ import json
 from datetime import datetime
 
 RED = redis.ConnectionPool(host='redis_01',port=6379,db=0)
+#RED = redis.ConnectionPool(host='tuchfarber.com',port=6379,db=0)
 LENGTH_OF_PREG = 280
 
 @get('/api/test')
@@ -14,7 +15,9 @@ def index():
 def return_date(date):
     redis_server = redis.Redis(connection_pool=RED)
     data = {}
+    data['data'] = {}
     response = redis_server.lrange('dates:' + date, 0, -1)
-    data['2000-05-05'] = response[0].decode("utf-8")
+    data['data']['detail'] = response[0].decode("utf-8")
+    data['data']['day'] = date
     return json.dumps(data)
  
