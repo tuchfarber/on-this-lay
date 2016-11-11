3026bff6a896dbeba9ded0b9a7686119474f9857
archibald <archibald@Squelch>
2016-11-10 21:39:25 -0500

    Added euphamisms. Removed styling to fix later.

--- front_end/home/home.js
+++ front_end/home/home.js
@@ -1,5 +1,14 @@
 'use strict';
 
+var euphs = [
+    "banging","doing it","bumping uglies","taking a trip to pound town", "knocking boots",
+    "doing the no pants dance", "sliming the banana","filling the cream donut", "boning",
+    "stuffing the taco","riding the skin bus into tuna town","spearing the bearded clam",
+    "boinking","playing hide the canoli","parking the beef bus in tuna town","humping",
+    "fornicating","shagging","fucking","putting sour cream in the burrito",
+    "putting ranch dressing in the hidden valley"
+    ]
+
 angular.module('myApp.home', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -28,6 +37,8 @@ angular.module('myApp.home', ['ngRoute'])
                 var details = response.data.data.detail;
                 if(details !== ''){
                     $scope.details = response.data.data.detail
+                    
+                    $scope.parents = "While your parent were busy " + euphs[Math.floor(Math.random() * euphs.length)] + ",";
                 }else{
                     $scope.details = "Your parents' coitus was not inspired by any important event."
                 }
